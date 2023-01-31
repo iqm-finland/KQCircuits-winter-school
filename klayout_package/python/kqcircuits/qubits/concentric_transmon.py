87f1c522e2acfc3b32b16b351a3a8c66f2e85910
Alessandro Landra <alessandro@meetiqm.com>
2023-01-31 17:07:50 +0200

    Completed code for Concentric transmon element

--- klayout_package/python/kqcircuits/qubits/concentric_transmon.py
+++ klayout_package/python/kqcircuits/qubits/concentric_transmon.py
@@ -17,9 +17,6 @@ from kqcircuits.util.parameters import Param, pdt, add_parameters_from
 @add_parameters_from(Element, n=180)
 class ConcentricTransmon(Qubit):
     """The PCell declaration for a concentric transmon.
-
-    WINTER SCHOOL 2023 ASSIGNMENT - replace ... with working code to implement the ConcentricTransmon qubit
-
     A concentric transmon consists of two islands, one inner and one outer, connected by a Josephson Junction/s. Multiple
     couplers can be defined. They can have custom waveguide impedance, size and shape.
     Each coupler has reference points, numbered starting from 1. Driveline can be connected to the drive port.
@@ -29,7 +26,7 @@ class ConcentricTransmon(Qubit):
     r_inner = Param(pdt.TypeDouble, "Internal island radius", 120, unit="μm",
                     docstring="Radius of the outer edge of the inner island")
     r_outer = Param(pdt.TypeDouble, "External island radius, measured at the outer edge", 250, unit="μm",
-                    docstring="Radius of the external coupler island")
+                    docstring="Radius of the external qubit island")
     outer_island_width = Param(pdt.TypeDouble, "Outer island radial width", 80, unit="μm",
                                docstring="Width of the external island")
     ground_gap = Param(pdt.TypeDouble, "Ground plane padding", 80, unit="μm")
@@ -80,129 +77,98 @@ class ConcentricTransmon(Qubit):
         self._add_ports()
 
     def _make_arc_island(self, island_outer_radius, island_width, swept_angle):
-        """WINTER SCHOOL 2023 - uncomment code below and fill in the missing parts"""
-
-        # # Generate a polygon arc of any size and angle
-
-        # angle_rad = math.radians(swept_angle)
-        # points_outside = arc_points(island_outer_radius, ..., ..., ...)
-        # points_inside = arc_points(island_outer_radius - island_width, ..., ..., ...)
-        # points = ...
-        # arc_island = ...
-
-        # return arc_island
-
-        return None  # WINTER SCHOOL 2023 - Remove this line when done
+        # Generate a polygon arc of any size and angle
+        angle_rad = math.radians(swept_angle)
+        points_outside = arc_points(island_outer_radius, -angle_rad / 2, angle_rad / 2, self.n)
+        points_inside = arc_points(island_outer_radius - island_width, angle_rad / 2, -angle_rad / 2, self.n)
+        points = points_inside + points_outside
+        arc_island = pya.DPolygon(points)
+        return arc_island
 
     def _make_qubit_islands(self):
-        """WINTER SCHOOL 2023 - uncomment code below and fill in the missing parts"""
-
         # Generate a region of the qubit shunting capacitor
-        # capacitor_islands = []
-        # # Inner circular island
-        # inner_island = circle_polygon(..., ...)
-        # capacitor_islands.append(inner_island)
-        # # Outer cylindrical island
-        # outer_island = self._make_arc_island(..., ..., ...)
-        # capacitor_islands.append(outer_island)
-
-        # return pya.Region([poly.to_itype(self.layout.dbu) for poly in capacitor_islands])
-
-        raise ValueError("Start the WINTER SCHOOL exercise by implementing _make_qubit_islands")  # WINTER SCHOOL 2023 - Remove this line when done
+        capacitor_islands = []
+        # Inner circular island
+        inner_island = circle_polygon(self.r_inner, self.n)
+        capacitor_islands.append(inner_island)
+        # Outer cylindrical island
+        outer_island = self._make_arc_island(self.r_outer, self.outer_island_width, 360)
+        capacitor_islands.append(outer_island)
+        return pya.Region([poly.to_itype(self.layout.dbu) for poly in capacitor_islands])
 
     def _make_coupler_island(self):
-        """WINTER SCHOOL 2023 - uncomment code below and fill in the missing parts"""
-
         # Generate the regions of the coupler islands.
         round_corner = 5
         coupler_islands_region = pya.Region()
-        # # Generate all the couplers in the same region
-        # for i, (c_angle, c_width, c_arc_ampl) in enumerate(
-        #         zip(..., ..., ...)):
-        #     coupler_island = self._make_arc_island(..., float(c_width), ...)
-        #     coupler_island_region = pya.Region(...).round_corners(
-        #         round_corner / self.layout.dbu, round_corner / self.layout.dbu, self.n).transformed(
-        #         pya.ICplxTrans(..., ..., ..., ..., ...))
-        #     coupler_islands_region += ...
-
+        # Generate all the couplers in the same region
+        for (c_angle, c_width, c_arc_ampl) in zip(self.couplers_angle, self.couplers_width,
+                                                  self.couplers_arc_amplitude):
+            coupler_island = self._make_arc_island(self.couplers_r + float(c_width) / 2, float(c_width),
+                                                   float(c_arc_ampl))
+            coupler_island_region = pya.Region(coupler_island.to_itype(self.layout.dbu)).round_corners(
+                round_corner / self.layout.dbu, round_corner / self.layout.dbu, self.n).transformed(
+                pya.ICplxTrans(1, float(c_angle), False, 0, 0))
+            coupler_islands_region += coupler_island_region
         return coupler_islands_region
 
     def _make_waveguides(self):
-        """WINTER SCHOOL 2023 - uncomment code below and fill in the missing parts"""
-
         # Make the waveguides for each coupler with custom impedance and return the region
         waveguides_signal_region = pya.Region()
         waveguides_gap_region = pya.Region()
         # Add the waveguides inside the ground padding
         overlapping_margin = 0.5
-        # for i, (c_a, c_b, c_angle) in enumerate(zip(..., ..., ...)):
-        #     waveguide_signal = pya.Region([pya.DPolygon([
-        #         pya.DPoint(...+overlapping_margin, float(c_a) / 2),
-        #         pya.DPoint(..., float(c_a) / 2),
-        #         pya.DPoint(..., -float(c_a) / 2),
-        #         pya.DPoint(...+overlapping_margin, -float(c_a) / 2),
-        #     ]).to_itype(self.layout.dbu)]).transformed(pya.ICplxTrans(1, ..., False, 0, 0))
-        #     waveguide_gap = pya.Region([pya.DPolygon([
-        #         pya.DPoint(self.x_end, ...),
-        #         pya.DPoint(self.couplers_r, ...),
-        #         pya.DPoint(self.couplers_r, ...),
-        #         pya.DPoint(self.x_end, ...),
-        #     ]).to_itype(self.layout.dbu)]).transformed(pya.ICplxTrans(1, ..., False, 0, 0))
-        #     waveguides_signal_region += waveguide_signal
-        #     waveguides_gap_region += waveguide_gap
+        for (c_a, c_b, c_angle) in zip(self.couplers_a, self.couplers_b, self.couplers_angle):
+            waveguide_signal = pya.Region([pya.DPolygon([
+                pya.DPoint(self.x_end + overlapping_margin, float(c_a) / 2),
+                pya.DPoint(self.couplers_r, float(c_a) / 2),
+                pya.DPoint(self.couplers_r, -float(c_a) / 2),
+                pya.DPoint(self.x_end + overlapping_margin, -float(c_a) / 2),
+            ]).to_itype(self.layout.dbu)]).transformed(pya.ICplxTrans(1, float(c_angle), False, 0, 0))
+            waveguide_gap = pya.Region([pya.DPolygon([
+                pya.DPoint(self.x_end, float(c_a) / 2 + float(c_b)),
+                pya.DPoint(self.couplers_r, float(c_a) / 2 + float(c_b)),
+                pya.DPoint(self.couplers_r, -float(c_a) / 2 - float(c_b)),
+                pya.DPoint(self.x_end, -float(c_a) / 2 - float(c_b)),
+            ]).to_itype(self.layout.dbu)]).transformed(pya.ICplxTrans(1, float(c_angle), False, 0, 0))
+            waveguides_signal_region += waveguide_signal
+            waveguides_gap_region += waveguide_gap
         return waveguides_signal_region, waveguides_gap_region
 
     def _add_junction(self, region):
-        """WINTER SCHOOL 2023 - uncomment code below and fill in the missing parts"""
-
-        # # Add the junction to the qubit islands
-        # squid_origin = arc_points(..., ..., 2 * math.pi, self.n, pya.DPoint(0, 0))[0]
-        # squid_transf = pya.DCplxTrans(..., ..., ..., ...)
-        # self.produce_squid(squid_transf)
-        # squid_distance_from_centre = self.refpoints['squid_port_common'].distance(self.refpoints['base'])
-        # # Connect the junction to the inner island
-        # squid_connection = pya.Region(squid_transf * pya.DPolygon([
-        #     pya.DPoint(-4, 0),
-        #     pya.DPoint(-4, -squid_distance_from_centre - 0.5),
-        #     pya.DPoint(4, -squid_distance_from_centre - 0.5),
-        #     pya.DPoint(4, 0)]
-        # ).to_itype(self.layout.dbu))
-        # region += ...
-
-        return None  # WINTER SCHOOL 2023 - Remove this line when done
+        # Add the junction to the qubit islands
+        squid_origin = \
+            arc_points(self.r_outer - self.outer_island_width, self.squid_angle * math.pi / 180, 2 * math.pi, self.n,
+                       pya.DPoint(0, 0))[0]
+        squid_transf = pya.DCplxTrans(1, self.squid_angle + 90, False, squid_origin)
+        self.produce_squid(squid_transf)
+        squid_distance_from_centre = self.refpoints['squid_port_common'].distance(self.refpoints['base'])
+        # Connect the junction to the inner island
+        squid_connection = pya.Region(squid_transf * pya.DPolygon([
+            pya.DPoint(-4, 0),
+            pya.DPoint(-4, -squid_distance_from_centre - 0.5),
+            pya.DPoint(4, -squid_distance_from_centre - 0.5),
+            pya.DPoint(4, 0)]
+        ).to_itype(self.layout.dbu))
+        region += squid_connection
 
     def _make_ground_region(self):
-        """WINTER SCHOOL 2023 - uncomment code below and fill in the missing parts"""
-
-        # # Generate the ground region as a filled circle of the maximum size
-        # n_points = self.n
-        # return pya.Region(... .to_itype(self.layout.dbu))
-
-        return pya.Region()  # WINTER SCHOOL 2023 - Remove this line when done
+        # Generate the ground region as a filled circle of the maximum size
+        return pya.Region(circle_polygon(self.x_end, self.n).to_itype(self.layout.dbu))
 
     def _add_ports(self):
-        """WINTER SCHOOL 2023 - uncomment code below and fill in the missing parts"""
-
-        # # Add couplers ports
-        # for i, c_angle in enumerate(map(float, self.couplers_angle)):
-        #     coupler_origin = arc_points(..., ..., 2 * math.pi, self.n, pya.DPoint(0, 0))[0]
-        #     coupler_transf = pya.DCplxTrans(1, ..., False, ...)
-        #     self.add_port(f"coupler_{i + 1}", ...,
-        #                   direction=pya.DVector(coupler_transf * pya.DPoint(0, 0)))
-        # # Add driveline port
-        # drive_origin = arc_points(..., ..., 2 * math.pi, self.n,
-        #                           pya.DPoint(0, 0))[0]
-        # drive_transf = pya.DCplxTrans(1, ..., False, ...)
-        # self.add_port("drive", ..., direction=pya.DVector(drive_transf * pya.DPoint(0, 0)))
-
-        return None  # WINTER SCHOOL 2023 - Remove this line when done
+        # Add couplers ports
+        for i, c_angle in enumerate(map(float, self.couplers_angle)):
+            coupler_origin = arc_points(self.x_end, c_angle * math.pi / 180, 2 * math.pi, self.n, pya.DPoint(0, 0))[0]
+            coupler_transf = pya.DCplxTrans(1, 90 + c_angle, False, coupler_origin)
+            self.add_port(f"coupler_{i + 1}", coupler_transf * pya.DPoint(0, 0),
+                          direction=pya.DVector(coupler_transf * pya.DPoint(0, 0)))
+        # Add driveline port
+        drive_origin = arc_points(self.drive_distance, self.drive_angle * math.pi / 180, 2 * math.pi, self.n,
+                                  pya.DPoint(0, 0))[0]
+        drive_transf = pya.DCplxTrans(1, 90 + self.drive_angle, False, drive_origin)
+        self.add_port("drive", drive_transf * pya.DPoint(0, 0), direction=pya.DVector(drive_transf * pya.DPoint(0, 0)))
 
     def _get_protection_region(self, region):
-        """WINTER SCHOOL 2023 - uncomment code below and fill in the missing parts"""
-
-        # # Region which we don't want to cover with the automatically generated ground grid
-        # protection_region = region.sized(...)
-
-        # return protection_region
-
-        return pya.Region()  # WINTER SCHOOL 2023 - Remove this line when done
+        # Region which we don't want to cover with the automatically generated ground grid
+        protection_region = region.sized(self.margin / self.layout.dbu, self.margin / self.layout.dbu, 2)
+        return protection_region
